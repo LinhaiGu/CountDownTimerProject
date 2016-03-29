b7dc28f309492ae28c3cf608a33a82c4c3bedfda
glh09896 <glh09896@ly.com>
2016-03-29 12:46:06 +0800

    顾林海-删除一些无用的代码

--- CountDownTimerProject/gen/com/example/countdowntimerproject/R.java
+++ CountDownTimerProject/gen/com/example/countdowntimerproject/R.java
@@ -25,10 +25,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int count_down_timer_view=0x7f080001;
-        public static final int second_view=0x7f080002;
-        public static final int tv_time=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int count_down_timer_view=0x7f080000;
+        public static final int second_view=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
